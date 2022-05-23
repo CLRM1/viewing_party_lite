5b5147b921e000ef80d4ea2690a911ad898ecff1
Chris Romero <chrisleeromero1@gmail.com>
2022-05-23 14:28:10 -0600

    refactor: add validation for password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     validates_presence_of :name
     validates_presence_of :email
+    validates_presence_of :password_digest
     validates_uniqueness_of :email
+
     has_secure_password
 end
