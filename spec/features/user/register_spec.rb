b9c5d9e194de8bb4ba9c768d68839b8371ffb934
Chris Romero <chrisleeromero1@gmail.com>
2022-05-23 13:59:37 -0600

    feat: password_digest test model test passes

--- spec/features/user/register_spec.rb
+++ spec/features/user/register_spec.rb
@@ -1,32 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "register page" do
-    # it  'has a page to register' do
-    #
-    #     visit '/register'
-    #
-    #     fill_in "name",	with: "Rupert"
-    #     fill_in "email",	with: "rupert@gmail.com"
-    #     click_button 'Register'
-    #
-    #     rup = User.first
-    #     expect(current_path).to eq("/users/#{rup.id}")
-    #     expect(page).to have_content("Rupert's Dashboard")
-    # end
 
-    it  'has a page to register' do
+  describe 'happy path' do
+    it 'has a page to register' do
+      visit '/register'
 
-        visit '/register'
+      fill_in "name",	with: "Rupert"
+      fill_in "email",	with: "rupert@gmail.com"
+      fill_in "password",	with: "123"
+      fill_in "password_confirmation",	with: "123"
+      click_button 'Register'
 
-        fill_in "name",	with: "Rupert"
-        fill_in "email",	with: "rupert@gmail.com"
-        fill_in "password",	with: "123"
-        fill_in "password_confirmation",	with: "123"
-        click_button 'Register'
-
-        rup = User.first
-        expect(current_path).to eq("/users/#{rup.id}")
-        expect(page).to have_content("Rupert's Dashboard")
+      rup = User.first
+      expect(current_path).to eq("/users/#{rup.id}")
+      expect(page).to have_content("Rupert's Dashboard")
     end
-
+  end
 end
