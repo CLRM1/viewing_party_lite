80bfbeed11ed8914f9386205a857f18358e006dc
Chris Romero <chrisleeromero1@gmail.com>
2022-05-23 13:30:15 -0600

    feat: register new user test with password and confirmation passes

--- spec/features/user/register_spec.rb
+++ spec/features/user/register_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "register page" do 
-    it  'has a page to register' do 
+RSpec.describe "register page" do
+    # it  'has a page to register' do
+    #
+    #     visit '/register'
+    #
+    #     fill_in "name",	with: "Rupert"
+    #     fill_in "email",	with: "rupert@gmail.com"
+    #     click_button 'Register'
+    #
+    #     rup = User.first
+    #     expect(current_path).to eq("/users/#{rup.id}")
+    #     expect(page).to have_content("Rupert's Dashboard")
+    # end
+
+    it  'has a page to register' do
 
         visit '/register'
-        
-        fill_in "name",	with: "Rupert" 
-        fill_in "email",	with: "rupert@gmail.com" 
+
+        fill_in "name",	with: "Rupert"
+        fill_in "email",	with: "rupert@gmail.com"
+        fill_in "password",	with: "123"
+        fill_in "password_confirmation",	with: "123"
         click_button 'Register'
-        
+
         rup = User.first
         expect(current_path).to eq("/users/#{rup.id}")
         expect(page).to have_content("Rupert's Dashboard")
